63b5115610ff2507be530e9cbd98a6f7d0d31d16
Josh Osborne <Josh.Osborne@devmounta.in>
2022-10-06 21:34:30 -0600

    alert try

--- public/index.js
+++ public/index.js
@@ -50,7 +50,7 @@ function displayDishes(arr) {
     for (let i = 0; i < arr.length; i++) {
         dishItem(arr[i])
         console.log(arr[i])
-        if(arr[i] === undefined) {
+        if(arr[i] === "") {
             return alert ("Please add a valid dish choice")
         }
     }
